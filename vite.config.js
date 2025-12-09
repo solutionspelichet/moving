2a84e97d9d58d20f53aa5ffc3b6a7b48ff1124cf
solutionspelichet <solutions.pelichet@gmail.com>
2025-12-09 11:04:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Remplacer 'votre-nom-de-repo' par le nom de votre projet GitHub
-  // Exemple: si votre URL est github.com/Jean/demenagement, mettez '/demenagement/'
+  // ATTENTION : Doit correspondre exactement au nom de votre dépôt GitHub (entre slashs)
   base: '/moving/', 
 })
